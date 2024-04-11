a3ac63e3fc8df86b8e70215a499f88050c4ae7a7
Мария Плетнева <=>
2024-04-11 23:20:25 +0500

    added check palindrome business logic

---
+++ BusinessLogic/PalindromeService.cs
@@ -0,0 +1,19 @@
+﻿using ServerSide.BusinessLogic.Interfaces;
+using ServerSide.Controllers.Interfaces;
+
+namespace ServerSide.BusinessLogic
+{
+    public class PalindromeService : IPalindromeService
+    {
+        IPalindromeValidator _validator { get; set; }
+        public PalindromeService(IPalindromeValidator validator)
+        {
+            _validator = validator;
+        }
+
+        public Task<bool> IsPalindrome(string value)
+        {
+            return Task.Run(() => _validator.IsValid(value));
+        }
+    }
+}
