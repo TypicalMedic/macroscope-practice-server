d88e4acd827ae82d97374655d0e14f10a9e11172
Мария Плетнева <=>
2024-04-12 12:39:31 +0500

    added concurrent rate limiter

--- Program.cs
+++ Program.cs
@@ -1,11 +1,29 @@
+using Microsoft.AspNetCore.RateLimiting;
 using ServerSide.BusinessLogic;
 using ServerSide.BusinessLogic.Interfaces;
 using ServerSide.Controllers.Interfaces;
 using ServerSide.PalindromeValidator;
+using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
+ConfigurationManager configuration = builder.Configuration;
+int maxConnections;
+if (!int.TryParse(configuration["requestlimit"], out maxConnections) || maxConnections <= 0)
+{
+    Console.WriteLine("Error: Command line argument 'requestlimit' should be positive int");
+    Environment.Exit(1);
+}
+
 // Add services to the container.
+var concurrencyPolicy = "Concurrency";
+builder.Services.AddRateLimiter(_ => _
+    .AddConcurrencyLimiter(policyName: concurrencyPolicy, options =>
+    {
+        options.PermitLimit = maxConnections; // максимальное количество текущих подключений
+        options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        options.QueueLimit = 0; // в очереди никто не может ждать, сразу отправляем 502
+    }));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -23,6 +41,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseRateLimiter();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
