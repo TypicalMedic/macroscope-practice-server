48fe756d9bf2e15ec44f64fd06a40832d8235754
Мария Плетнева <=>
2024-04-14 17:50:51 +0500

    migrated serverside project away from solution

---
+++ ServerSide/Tests/PalindromeServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace macroscopTestTask.Test
+{
+    [TestClass]
+    internal class PalindromeServiceTests
+    {
+        [TestMethod]
+        public void TestEmptyString() { }
+
+        [TestMethod]
+        public void TestStringLength1() { }
+
+        [TestMethod]
+        public void TestStringPalindrome() { }
+
+        [TestMethod]
+        public void TestStringNotPalindrome() { }
+    }
+}
